b445cdc07039097033b3f11d9a0503eb47868659
Rahul D Shetty <35rahuldshetty@gmail.com>
2023-11-25 23:12:10 +0530

    completed chapter1

--- chap1/9_string_rotation.py
+++ chap1/9_string_rotation.py
@@ -14,3 +14,32 @@ sys.path.append(parent)
 from test_utils import TestSuite
 
 
+ts = TestSuite() \
+    .add_test(("waterbottle", "erbottlewat"), True) \
+    .add_test(("abcde", "abced"), False)
+
+
+def check1(input):
+    s, goal = input
+    if len(s) != len(goal):
+        return False
+    
+    n = len(s)
+
+    for i in range(n):
+        a, b = s[:i], s[i:]
+        if b + a == goal:
+            return True
+    
+    return False
+
+
+def check2(input):
+    s, goal = input
+    if len(s) != len(goal):
+        return False
+    return goal in s + s 
+
+
+ts.run(check1)
+ts.run(check2)
